bf31b08fe6624f679ed672a336a39df7fe8901c2
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-10-24 12:56:08 +0000

    part B

--- Main.java
+++ Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
-   }
-}
+
+     // CODE for part B
+      String highSchoolName = "Birch Wathen Lenox";
+       double priceDrink = 1.50;
+       double priceCandy = 1.25;
+       double priceHotDog = 2.75;
+       double priceHumburger = 3.50;
+      System.out.println("highSchoolName");
+     
+      // RECEIPT PRINTOUT SECTION
+     System.out.println("**************************************");
+     System.out.println("*                                    *");
+     System.out.println("*     " + highSchoolName + " Snack Bar          *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink  ..........$ "priceDrink"         *");                      
+     System.out.println("*     Candy ..........$1.25          *");     
+     System.out.println("*     Hot Dog ........$2.75          *");     
+     System.out.println("*     Hamburger ......$3.50          *");     
+     System.out.println("*                                    *");    
+     System.out.println("**************************************");
+  }
+  }
+
+   
+
