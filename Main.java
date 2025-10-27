cd0b928916d1bbd89465131aefc5bb5ddc7b52ff
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-10-27 14:31:41 +0000

    finishing part C

--- Main.java
+++ Main.java
@@ -9,16 +9,26 @@ public class Main {
        double priceHotDog = 2.75;
        double priceHumburger = 3.50;
       System.out.println("highSchoolName");
+
+      int orderNumber = (Math.random(12) *100);
+      int numOfDrinkOrder = (Math.random(10) *100);
+      int numOfCandiesOrder = (Math.random(32) *100);
+      int numOfHotDogOrder = (Math.random() *100);
+      int numOfHamburgersOrder = (Math.random() *100);
+      double taxRate = 0.08;
+      double subtotal = "$1.50" * 12 ;
+      double totalTax = subtotal * taxRate;
+      double total = subtotal + totalTax;
      
       // RECEIPT PRINTOUT SECTION
      System.out.println("**************************************");
      System.out.println("*                                    *");
-     System.out.println("*     " + highSchoolName + " Snack Bar          *");
+     System.out.println("*     " + highSchoolName + " Snack Bar            *");
      System.out.println("*                                    *");
-     System.out.println("*     Drink  ..........$ "priceDrink"         *");                      
-     System.out.println("*     Candy ..........$1.25          *");     
-     System.out.println("*     Hot Dog ........$2.75          *");     
-     System.out.println("*     Hamburger ......$3.50          *");     
+     System.out.println("*     Drink ..........$ " + priceDrink +"         *");                      
+     System.out.println("*     Candy ..........$ " + priceCandy +"         *");     
+     System.out.println("*     Hot Dog ........$ " + priceHotDog +"        *");     
+     System.out.println("*     Hamburger ......$ " + priceHumburger +"     *");     
      System.out.println("*                                    *");    
      System.out.println("**************************************");
   }
