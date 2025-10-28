335855a5a2a17ccf0242c35447ff467983c1430e
SohailaN12 <sohaila.noori2027@gbwl.org>
2025-10-28 15:05:43 +0000

    part D

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class Main {
-
    public static void main(String []args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("amount of drinks");
+      int xdrink = input.nextInt();
+      System.out.println("amount of candy");
+      int xcandy = input.nextInt();
+      System.out.println("amount of hotdog");
+      int xhotdog = input.nextInt();
+      System.out.println("amount of hamburgers");
+      int xhamburgers = input.nextInt();
+      
+
+
 
      // CODE for part B
       String highSchoolName = "Birch Wathen Lenox";
@@ -10,26 +22,33 @@ public class Main {
        double priceHumburger = 3.50;
       System.out.println("highSchoolName");
 
-      int orderNumber = (Math.random(12) *100);
-      int numOfDrinkOrder = (Math.random(10) *100);
-      int numOfCandiesOrder = (Math.random(32) *100);
-      int numOfHotDogOrder = (Math.random() *100);
-      int numOfHamburgersOrder = (Math.random() *100);
+      // CODE for part C  
+      int orderNumber = (int) (Math.random() *100);
+      int numDrink = xdrink;
+      int numCandy =xcandy;
+      int numHotDog = xhotdog;
+      int numHamburgers = xhamburgers;
+
       double taxRate = 0.08;
-      double subtotal = "$1.50" * 12 ;
+      double subtotal = priceDrink*numDrink+priceCandy*numCandy+priceHotDog*numHotDog+priceHumburger*numHamburgers;
       double totalTax = subtotal * taxRate;
       double total = subtotal + totalTax;
+
+      
+
      
       // RECEIPT PRINTOUT SECTION
      System.out.println("**************************************");
      System.out.println("*                                    *");
      System.out.println("*     " + highSchoolName + " Snack Bar            *");
      System.out.println("*                                    *");
-     System.out.println("*     Drink ..........$ " + priceDrink +"         *");                      
-     System.out.println("*     Candy ..........$ " + priceCandy +"         *");     
-     System.out.println("*     Hot Dog ........$ " + priceHotDog +"        *");     
-     System.out.println("*     Hamburger ......$ " + priceHumburger +"     *");     
-     System.out.println("*                                    *");    
+     System.out.println("*     Order # " + orderNumber +"          *"); 
+     System.out.println("*     Drink ..........$ " + priceDrink + " #" + xdrink + "         *");                      
+     System.out.println("*     Candy ..........$ " + priceCandy +  " #" + xcandy + "  *");     
+     System.out.println("*     Hot Dog ........$ " + priceHotDog +" #" + xhotdog + "     *");     
+     System.out.println("*     Hamburger ......$ " + priceHumburger +" #" + xhamburgers +"  *"); 
+     System.out.println("*     Total ........$ " + subtotal +"        *");     
+     System.out.println("*                                      *");    
      System.out.println("**************************************");
   }
   }
